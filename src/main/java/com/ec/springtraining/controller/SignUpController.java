16e2d4db5e8aa17533f1f9df8222520d5dd774e1
“shomakobayashi” <pipipipiii0001@yahoo.co.jp>
2024-11-28 21:31:48 +0900

    message.propeertiesを作成 HTMLのベタガキを移動

--- src/main/java/com/ec/springtraining/controller/SignUpController.java
+++ src/main/java/com/ec/springtraining/controller/SignUpController.java
@@ -5,15 +5,32 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
 
 @Controller
+@RequestMapping("/user")
 public class SignUpController {
 
     @Autowired
     private UserApplicationService userApplicationService;;
 
-    @GetMapping("/user")
+    @GetMapping("/signup")
     public String getSignUp(Model model) {
+        //性別を取得
+        Map<String, Integer> genderMap = userApplicationService.getGenderMap();
+        model.addAttribute("genderMap", genderMap);
 
+        // ユーザ登録画面に遷移
+        return "user/signup";
+    }
+
+    /**ユーザー登録処理**/
+    @PostMapping
+    public String postSignUp() {
+        //ログイン画面にリダイレクト
+        return "redirect:/login";
     }
 }
--- src/main/java/com/ec/springtraining/service/UserApplicationService.java
+++ src/main/java/com/ec/springtraining/service/UserApplicationService.java
@@ -1,18 +1,26 @@
 package com.ec.springtraining.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 
 import java.util.LinkedHashMap;
+import java.util.Locale;
 import java.util.Map;
 
 @Service
 public class UserApplicationService {
 
+    @Autowired
+    private MessageSource messageSource;
+
     public Map<String, Integer> getGenderMap() {
         Map<String, Integer> genderMap = new LinkedHashMap<>();
-        genderMap.put("男性", 1);
-        genderMap.put("女性", 2);
+        String male = messageSource.getMessage("male", null, Locale.JAPAN);
+        String female = messageSource.getMessage("female", null, Locale.JAPAN);
+        genderMap.put(male, 1);
+        genderMap.put(female, 2);
         return genderMap;
     }
 
